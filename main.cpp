4fbf752a15e66bbe40cfaa6b9f0367089ac2250d
SleepyCodingBear <58487872+SleepyCodingBear@users.noreply.github.com>
2020-11-30 19:25:22 +0100

    testDreieck

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main() {
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(1280, 960, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -26,12 +26,36 @@ int main() {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glMatrixMode( GL_PROJECTION ); // Stack für Projektionsmatrix als
+        // aktiven Matrixstack setzen
+        glLoadIdentity();              // Identitätsmatrix auf den Stack laden
+
+        // Eine orthogonale Projektionsmatrix zum Stack
+        // dazu multiplizieren.
+        glOrtho( 0, 800, 600, 0, -1, 1 );
+        
+        glBegin( GL_TRIANGLES ); // Wir wollen ein Dreieck zeichnen
+        glColor3f(1,0,0);      // Ab jetzt werden alle gezeichneten Punkte rot
+        glVertex3f(400,100,0); // Der erste Eckpunkt ist mittig und 100 Pixel
+        // vom oberen Rand entfernt
+
+        glColor3f(0,1,0);      // Ab jetzt werden alle gezeichneten Punkte grün
+        glVertex3f(750,500,0); // Der zweite Eckpunkt ist 50 Pixel vom rechten
+        // und 100 Pixel vom unteren Rand entfernt
+
+        glColor3f(0,0,1);      // Ab jetzt werden alle gezeichneten Punkte blau
+        glVertex3f(50,500,0);  // Der dritte Eckpunkt ist 50 Pixel vom linken
+        // und 100 Pixel vom unteren Rand entfernt
+        glEnd();
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
         /* Poll for and process events */
         glfwPollEvents();
 
+        printf("do_it");
+        _sleep(1000);
     }
 
     glfwTerminate();
