4e935552649e698a16b4a6829a0d1ae6f214ff56
SleepyCodingBear <58487872+SleepyCodingBear@users.noreply.github.com>
2020-12-22 20:28:26 +0100

    include

--- include/view/View.h
+++ include/view/View.h
@@ -2,7 +2,7 @@
 // Created by basti on 11.12.2020.
 //
 #include "../../libs/glfw-3.3.2/include/GLFW/glfw3.h"
-#include <gl/gl.h>
+#include <gl/GL.h>
 #include "../model/Model.h"
 
 #ifndef BOSCONIAN_VIEW_H
