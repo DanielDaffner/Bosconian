151cb04f0c57f09cceaf334d1ea96448e1ee807a
SleepyCodingBear <58487872+SleepyCodingBear@users.noreply.github.com>
2021-03-04 23:27:45 +0100

    135

--- src/audio/Sound.cpp
+++ src/audio/Sound.cpp
@@ -1,132 +1,132 @@
+////
+//// Created by Daniel on 02.03.2021.
+////
 //
-// Created by Daniel on 02.03.2021.
 //
-
-
-#include <fstream>
-#include <iostream>
-#include "../../include/audio/Sound.h"
-#include "../../libs/openal-soft/include/AL/al.h"
-#include "../../libs/openal-soft/include/AL/alc.h"
-
-bool isBigEndian(){
-    int a = 1;
-    return !((char*)&a)[0];
-}
-
-int convertToInt(char* buffer, int len){
-    int a=0;
-    if(!isBigEndian()) {
-        for (int i = 0; i < len; i++) {
-            ((char *) &a)[i] = buffer[i];
-        }
-    }
-    else {
-        for (int i=0;i<len;i++) {
-            ((char*)&a) [3-1]=buffer[i];
-        }
-    }
-    return a;
-}
-
-char* loadWAV(const char* fn, int& chan, int& samplerate, int& bps, int& size){
-    char buffer[4];
-    std::ifstream in( fn, std::ios::binary);
-    in.read(buffer,4);
-    if(strncmp(buffer,"RIFF",4)!=0) {
-        std::cout << "this is not a valid WAVE file" << std::endl;
-        std::cout << buffer[0] << buffer[1] << buffer[2] << buffer[3] << std::endl;
-        //return NULL;
-    }
-    in.read(buffer,4);
-    in.read(buffer,4);  //WAVE
-    in.read(buffer,4);  //fmt
-    in.read(buffer,4);  //16
-    in.read(buffer,2);  //1
-    in.read(buffer,2);
-    chan=convertToInt(buffer,2);
-    in.read(buffer,4);
-    samplerate=convertToInt(buffer,4);
-    in.read(buffer,4);
-    in.read(buffer,2);
-    in.read(buffer,2);
-    bps=convertToInt(buffer,2);
-    in.read(buffer,4);  //data
-    in.read(buffer,4);
-    size=convertToInt(buffer,4);
-    std::cout << size << std::endl;
-    char* data= new char[size];
-
-    in.read(data,size);
-
-    return data;
-
-}
-
-int test(){
-    //test
-    if (isBigEndian()){
-        std::cout << "big endian" << std::endl;
-
-    }
-    else {
-        std::cout << "little endian" << std::endl;
-    }
-
-    // start
-    int channel,sampleRate,bps,size;
-
-    char* data=loadWAV("test.wav", channel, sampleRate,bps,size);
-    ALCdevice* device=alcOpenDevice(NULL);
-    if(device==NULL)
-    {
-        std::cout << "cannot open sound card" << std::endl;
-        return 0;
-    }
-    ALCcontext* context=alcCreateContext(device, NULL);
-    if(context=NULL){
-        std::cout << "cannot open context" << std::endl;
-        return 0;
-    }
-    alcMakeContextCurrent(context);
-
-    unsigned int bufferid,format;
-    alGenBuffers(1, &bufferid);
-    if(channel==1)
-    {
-        if(bps==8)
-        {
-            AL_FORMAT_MONO8;
-        }
-        else{
-            format=AL_FORMAT_MONO16;
-        }
-
-    }
-    else{
-        if(bps==8){
-            format=AL_FORMAT_STEREO8;
-        }
-        else{
-            format=AL_FORMAT_STEREO16;
-        }
-    }
-
-    alBufferData(bufferid, format, data, size,sampleRate);
-
-    unsigned int sourceid;
-    alGenSources(1,&sourceid);
-    alSourcei(sourceid,AL_BUFFER,bufferid);
-    alSourcePlay(sourceid);
-    _sleep(5000);
-
-    alDeleteSources(1,&sourceid);
-    alDeleteBuffers(1,&bufferid);
-
-    alcDestroyContext(context);
-    alcCloseDevice(device);
-
-    delete[] data;
-
-    return 0;
-}
+//#include <fstream>
+//#include <iostream>
+//#include "../../include/audio/Sound.h"
+//#include "../../libs/openal-soft/include/AL/al.h"
+//#include "../../libs/openal-soft/include/AL/alc.h"
+//
+//bool isBigEndian(){
+//    int a = 1;
+//    return !((char*)&a)[0];
+//}
+//
+//int convertToInt(char* buffer, int len){
+//    int a=0;
+//    if(!isBigEndian()) {
+//        for (int i = 0; i < len; i++) {
+//            ((char *) &a)[i] = buffer[i];
+//        }
+//    }
+//    else {
+//        for (int i=0;i<len;i++) {
+//            ((char*)&a) [3-1]=buffer[i];
+//        }
+//    }
+//    return a;
+//}
+//
+//char* loadWAV(const char* fn, int& chan, int& samplerate, int& bps, int& size){
+//    char buffer[4];
+//    std::ifstream in( fn, std::ios::binary);
+//    in.read(buffer,4);
+//    if(strncmp(buffer,"RIFF",4)!=0) {
+//        std::cout << "this is not a valid WAVE file" << std::endl;
+//        std::cout << buffer[0] << buffer[1] << buffer[2] << buffer[3] << std::endl;
+//        //return NULL;
+//    }
+//    in.read(buffer,4);
+//    in.read(buffer,4);  //WAVE
+//    in.read(buffer,4);  //fmt
+//    in.read(buffer,4);  //16
+//    in.read(buffer,2);  //1
+//    in.read(buffer,2);
+//    chan=convertToInt(buffer,2);
+//    in.read(buffer,4);
+//    samplerate=convertToInt(buffer,4);
+//    in.read(buffer,4);
+//    in.read(buffer,2);
+//    in.read(buffer,2);
+//    bps=convertToInt(buffer,2);
+//    in.read(buffer,4);  //data
+//    in.read(buffer,4);
+//    size=convertToInt(buffer,4);
+//    std::cout << size << std::endl;
+//    char* data= new char[size];
+//
+//    in.read(data,size);
+//
+//    return data;
+//
+//}
+//
+//int test(){
+//    //test
+//    if (isBigEndian()){
+//        std::cout << "big endian" << std::endl;
+//
+//    }
+//    else {
+//        std::cout << "little endian" << std::endl;
+//    }
+//
+//    // start
+//    int channel,sampleRate,bps,size;
+//
+//    char* data=loadWAV("test.wav", channel, sampleRate,bps,size);
+//    ALCdevice* device=alcOpenDevice(NULL);
+//    if(device==NULL)
+//    {
+//        std::cout << "cannot open sound card" << std::endl;
+//        return 0;
+//    }
+//    ALCcontext* context=alcCreateContext(device, NULL);
+//    if(context=NULL){
+//        std::cout << "cannot open context" << std::endl;
+//        return 0;
+//    }
+//    alcMakeContextCurrent(context);
+//
+//    unsigned int bufferid,format;
+//    alGenBuffers(1, &bufferid);
+//    if(channel==1)
+//    {
+//        if(bps==8)
+//        {
+//            AL_FORMAT_MONO8;
+//        }
+//        else{
+//            format=AL_FORMAT_MONO16;
+//        }
+//
+//    }
+//    else{
+//        if(bps==8){
+//            format=AL_FORMAT_STEREO8;
+//        }
+//        else{
+//            format=AL_FORMAT_STEREO16;
+//        }
+//    }
+//
+//    alBufferData(bufferid, format, data, size,sampleRate);
+//
+//    unsigned int sourceid;
+//    alGenSources(1,&sourceid);
+//    alSourcei(sourceid,AL_BUFFER,bufferid);
+//    alSourcePlay(sourceid);
+//    _sleep(5000);
+//
+//    alDeleteSources(1,&sourceid);
+//    alDeleteBuffers(1,&bufferid);
+//
+//    alcDestroyContext(context);
+//    alcCloseDevice(device);
+//
+//    delete[] data;
+//
+//    return 0;
+//}
